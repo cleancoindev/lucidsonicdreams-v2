81a0836ea4000ffc095e13539d2ab343451d2e17
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2022-12-29 15:34:23 -1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("./content/README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="lucidsonicdreams v2", 
+    version="0.01",
+    author="BigZaddy",
+    author_email="@gmail.com",
+    description="Syncs GAN-generated visuals to music",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.7',
+    url="https://github.com/DeFi-Coder-News-Letter/lucid-sonic-dreams",
+    download_url="https://github.com/DeFi-Coder-News-Letter/lucid-sonic-dreams/dist/lucid-sonic-dreams.tar.gz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['tensorflow==1.15.0',
+                      'librosa',
+                      'numpy',
+                      'moviepy',
+                      'Pillow',
+                      'tqdm',
+                      'scipy',
+                      'scikit-image',
+                      'pygit2',
+                      'gdown', 
+                      'mega.py',
+                      'requests',
+                      'pandas',
+                      'SoundFile']
+)
